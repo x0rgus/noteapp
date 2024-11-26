a8390cf7a93302b6bd0381de7c99613aa13e6199
Lucas Procópio <101903078+x0rgus@users.noreply.github.com>
2024-11-25 22:29:56 -0300

    :sparkles: created serializers / views / routes

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
-# Create your views here.
+# View to create a new user, anyone can call it to create a new user (ex: Unauthenticated users)
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class =  User
+    permission_classes = [AllowAny]
