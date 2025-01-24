08d6d614fd70b540534cf71ec76c244638cd0115
Lucas Procópio <101903078+x0rgus@users.noreply.github.com>
2025-01-24 17:59:38 -0300

    :sparkles: created create note area in home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -17,9 +17,59 @@ function Home() {
         .then((data) => {setNotes(data); console.log(data)})
         .catch((err) => alert(err));  
 
+    };
+
+    const deleteNote = (id) => {
+        api.delete(`/api/notes/delete/${id}`).then((res) => {
+            if (res.status === 204) alert("Note deleted!");
+            else alert("Failed to delete note");
+        }).catch((error) => alert(error));
+        getNotes();
+    };
+
+    const createNote = (e) =>{
+        e.preventDefault()
+        api.post("api/notes", {content, title}).then ((res) =>{
+            if (res.status === 201) alert("Note created!");
+            else alert("Failed to make note");
+        }).catch((err) => alert(err));
+        getNotes();
     }
+
     
-    return <div>Home</div>
+    return <div>
+        <div>
+            <h2>Notes</h2>
+
+        </div>
+        <h2>Create a Note</h2>
+        <form onSubmit={createNote}>
+            <label htmlFor="title">Title:</label>
+        <br/>
+        <input
+            type="text"
+            id="title"
+            name="title"
+            required
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+        />
+        <label htmlFor="content">Content:</label>
+        <br/>
+        <textarea 
+            id="content" 
+            name="content" 
+            required 
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+        ></textarea>
+        <br />
+        <input 
+            type="submit"
+            value="Submit"
+        ></input>
+        </form>
+    </div>
 }
 
 export default Home
