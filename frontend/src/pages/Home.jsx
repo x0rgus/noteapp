b114c8b1cd82619c48c1a63c68c4bc69409a15b0
Lucas Procópio <101903078+x0rgus@users.noreply.github.com>
2025-01-27 14:59:06 -0300

    :wrench: fixed path to delete notes, :sparkles: created view for notes in home page, button to delete notes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
 import api from "../api"
+import Note from "../components/Note"
 
 
 function Home() {
@@ -13,61 +14,63 @@ function Home() {
 
     const getNotes = () => {
         api.get("/api/notes/") // route defined in django to get notes from db
-        .then((res) => res.data)
-        .then((data) => {setNotes(data); console.log(data)})
-        .catch((err) => alert(err));  
+            .then((res) => res.data)
+            .then((data) => { setNotes(data); console.log(data) })
+            .catch((err) => alert(err));
 
     };
 
     const deleteNote = (id) => {
-        api.delete(`/api/notes/delete/${id}`).then((res) => {
+        api.delete(`/api/notes/delete/${id}/`).then((res) => {
             if (res.status === 204) alert("Note deleted!");
             else alert("Failed to delete note");
             getNotes();
         }).catch((error) => alert(error));
     };
 
-    const createNote = (e) =>{
+    const createNote = (e) => {
         e.preventDefault()
-        api.post("/api/notes/", {content, title}).then ((res) =>{
+        api.post("/api/notes/", { content, title }).then((res) => {
             if (res.status === 201) alert("Note created!");
             else alert("Failed to make note");
             getNotes();
         }).catch((err) => alert(err));
     };
 
-    
+
     return <div>
         <div>
             <h2>Notes</h2>
-
+            {notes.map((note) => (
+                <Note note={note} onDelete={deleteNote} key={note.id} />
+            ))}
         </div>
         <h2>Create a Note</h2>
         <form onSubmit={createNote}>
             <label htmlFor="title">Title:</label>
-        <br/>
-        <input
-            type="text"
-            id="title"
-            name="title"
-            required
-            onChange={(e) => setTitle(e.target.value)}
-            value={title}
-        />
-        <label htmlFor="content">Content:</label>
-        <br/>
-        <textarea 
-            id="content" 
-            name="content" 
-            required 
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-        ></textarea>
-        <br />
-        <input 
-            type="submit"
-            value="Submit"
-        ></input>
+            <br />
+            <input
+                type="text"
+                id="title"
+                name="title"
+                required
+                onChange={(e) => setTitle(e.target.value)}
+                value={title}
+            />
+            <label htmlFor="content">Content:</label>
+            <br />
+            <textarea
+                id="content"
+                name="content"
+                required
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
+            ></textarea>
+            <br />
+            <input
+                type="submit"
+                value="Submit"
+            ></input>
         </form>
     </div>
 }
